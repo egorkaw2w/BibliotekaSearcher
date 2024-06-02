afa4b96ee3c867d592f9230388d72b6f0fd54571
egorkaw2w <85741587+egorkaw2w@users.noreply.github.com>
2024-06-02 23:15:20 +0300

    Добавьте файлы проекта.

---
+++ BibliotekaSearcher/dllmain.cpp
@@ -0,0 +1,36 @@
+﻿
+#include "pch.h"
+
+#include <iostream>
+
+using namespace std;
+
+extern "C" __declspec(dllexport) bool FindCharacters(const char* inputString, const char* charactersToFind) {
+    string str = inputString;
+    string chars = charactersToFind;
+
+    for (char c : chars) {
+        if (str.find(c) == string::npos) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
